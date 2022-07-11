34405d1a7342933b7778856a8916b4a8d1ff76a9
mamuncseru <mamun.cseian@gmail.com>
2022-07-11 20:02:14 +0600

    Gamma Categorization

---
+++ GammaCategorization.py
@@ -0,0 +1,19 @@
+"""
+Gamma Categorization
+
+    -> Design patterns are typically split into three categories
+    -> This is called Gamma Categorization after Erich Gamma, one of GoF authors
+
+    -> Creational Patterns
+        --> Deal with the creation (construction) of objects
+        --> Explicit (constructor) vs implicit (DI, reflection, etc.)
+        --> Wholesale (single statement) vs. piecewise (step-by-step)
+
+    -> Structure Patterns
+        --> Concerned with the structure (e.g, class members)
+        --> Many patterns are wrappers that mimic the uderlying class interface
+        --> Stress the importance of good API design
+
+    -> Behavioral Patterns
+        --> They all different; no central theme
+"""
