fc3a53104f00424f4f257ec8016c059b5fc880f1
mamuncseru <mamun.cseian@gmail.com>
2022-07-28 09:23:25 +0600

    tiny little staticmethod factory method

--- Factories/factories.py
+++ Factories/factories.py
@@ -5,7 +5,7 @@ class CoordinateSystem(Enum):
     POLAR = 2
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
@@ -23,16 +23,18 @@ class Point:
 #             self.x = a * cos(b)
 #             self.y = a * sin(b)
 
-    @staticmethod
-    def new_cartesian_point(x, y):
-        return Point(x, y)
+    class PointFactory:
+        def new_cartesian_point(self, x, y):
+            return Point(x, y)
 
-    @staticmethod
-    def new_polar_point(rho, theta):
-        return Point(rho * cos(theta), rho * sin(theta))
+        def new_polar_point(self, rho, theta):
+            return Point(rho * cos(theta), rho * sin(theta))
+
+    
+    factory = PointFactory()
 
 
 if __name__ == '__main__':
     p = Point(2, 3)
-    p2 = Point.new_polar_point(1, 2)
+    p2 = Point.factory.new_polar_point(1, 2)
     print(p, p2)
